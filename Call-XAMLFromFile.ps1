7c4b74e57f34e0b65120c5e138d9329abfdd197b
Kris Gross <42305967+Jack0falltech@users.noreply.github.com>
2018-11-11 20:00:32 -0700

    Calls a XAML file into PowerShell

---
+++ Call-XAMLFromFile.ps1
@@ -0,0 +1,37 @@
+#===========================================================================
+# Gathers the XAML file
+#===========================================================================
+# Change the path to the path of your xaml file
+$XAMLPath = "$PSScriptroot\XAML\WpfWindow1.xaml"
+#===========================================================================
+$inputXML = Get-Content -Path "$XAMLPath"
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+#Read XAML
+$reader=(New-Object System.Xml.XmlNodeReader $xaml)
+try{
+    $mainForm=[Windows.Markup.XamlReader]::Load($reader)
+}catch{
+    Write-Warning "Unable to parse XML, with error: $($Error[0])"
+}
+#===========================================================================
+# Load XAML Objects In PowerShell
+#===========================================================================
+$xaml.SelectNodes("//*[@Name]") | ForEach-Object{ Set-Variable -Name "WPF_$($_.Name)" -Value $mainForm.FindName($_.Name) -ErrorAction Stop}
+#===========================================================================
+# Uses this to get the variables that are loaded into the script from the XAML
+# Function Get-FormVariables{
+# if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
+# write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+# get-variable WPF*
+# }
+# Get-FormVariables
+#===========================================================================
+
+# Marks the form as active
+[void]$Mainform.Activate()
+# Brings the form into focus
+[void]$Mainform.Focus()
+# Shows The form
+[void]$Mainform.ShowDialog()
